2d7f858233cf78489505745c2c61cfdb58eacc7b
Daniel Davis <deardanieldavis@gmail.com>
2020-09-28 22:45:34 -0400

    Ray fully tested

---
+++ src/lib/ray.spec.ts
@@ -0,0 +1,119 @@
+import anyTest, { TestInterface } from 'ava';
+import { Plane } from './plane';
+import { Point } from './point';
+import { Ray } from './ray';
+import { shapetypesSettings } from './settings';
+import { Vector } from './vector';
+
+
+const test = anyTest as TestInterface<{
+  angled: Ray;
+  flat: Ray;
+}>;
+
+test.beforeEach('Create test geometry', t => {
+  t.context.angled = new Ray(new Point(3,4), new Vector(3,4));
+  t.context.flat = new Ray(new Point(3,4), new Vector(1,0));
+});
+
+// -----------------------
+// CONSTRUCTOR
+// -----------------------
+test('Constructor: Sets correct from and direction', t => {
+  const ray = new Ray(new Point(3,4), new Vector(3,4));
+  t.true(ray.from.equals(new Point(3,4)));
+  t.true(ray.direction.isParallelTo(new Vector(3,4)));
+});
+
+
+// -----------------------
+// STATIC
+// -----------------------
+
+test('fromPoints: Creates correct direction', t => {
+  const ray = Ray.fromPoints(new Point(3,4), new Point(6, 8));
+  t.true(ray.from.equals(new Point(3,4)));
+  t.true(ray.direction.isParallelTo(new Vector(3,4)));
+});
+
+// -----------------------
+// GET
+// -----------------------
+test('from: Returns correct point', t => {
+  t.true(t.context.angled.from.equals(new Point(3,4)));
+});
+
+test('direction: Returns correct Vector', t => {
+  // Need to divide by 5 since length of vector is 5 and it was made into a unit.
+  t.true(t.context.angled.direction.equals(new Vector(3/5,4/5)));
+});
+
+// -----------------------
+// PUBLIC
+// -----------------------
+test('pointAt: Returns correct point', t => {
+  t.true(t.context.angled.pointAt(0).equals(new Point(3,4)));
+  t.true(t.context.angled.pointAt(5).equals(new Point(6,8)));
+  t.true(t.context.angled.pointAt(-5).equals(new Point(0,0)));
+});
+
+test('closestParameter: Returns correct parameter', t => {
+  t.is(t.context.angled.closestParameter(new Point(6,8)), 5);
+});
+test('closestParameter: Returns correct parameter depending on if onlyPositive or not', t => {
+  t.is(t.context.angled.closestParameter(new Point(0,0)), -5);
+  t.is(t.context.angled.closestParameter(new Point(0,0), false), -5);
+  t.is(t.context.angled.closestParameter(new Point(0,0), true), 0);
+});
+test('closestParameter: Returns correct parameter when point is off line', t => {
+  t.is(t.context.flat.closestParameter(new Point(4,0)), 1);
+});
+
+test('closestPoint: Returns correct point', t => {
+  t.true(t.context.angled.closestPoint(new Point(6,8)).equals(new Point(6,8)));
+});
+test('closestPoint: Returns correct point depending on if onlyPositive or not', t => {
+  t.true(t.context.angled.closestPoint(new Point(0,0)).equals(new Point(0,0)));
+  t.true(t.context.angled.closestPoint(new Point(0,0), false).equals(new Point(0,0)));
+  t.true(t.context.angled.closestPoint(new Point(0,0), true).equals(new Point(3,4)));
+});
+test('closestPoint: Returns correct point when point is off line', t => {
+  t.true(t.context.flat.closestPoint(new Point(4,0)).equals(new Point(4,4)));
+});
+
+test('withFrom: correctly replaces from', t => {
+  const ray = t.context.angled.withFrom(new Point(20, 30));
+  t.true(ray.from.equals(new Point(20,30)));
+});
+
+test('withDirection: correctly replaces direction', t => {
+  const ray = t.context.angled.withDirection(new Vector(20, 30));
+  t.true(ray.direction.isParallelTo(new Vector(20,30)));
+});
+
+// -----------------------
+// TRANSFORM
+// -----------------------
+
+test('changeBasis: correctly moves origin and axis', t => {
+  shapetypesSettings.invertY = false;
+  const from = Plane.worldXY();
+  const to = new Plane(new Point(3,4), new Vector(0, -1));
+  const ray = t.context.flat.changeBasis(from, to);
+  t.true(ray.from.equals(new Point(0, 0)));
+  t.true(ray.direction.equals(new Vector(0,1)));
+});
+
+test('rotate: correctly rotates the planes axis', t => {
+  shapetypesSettings.invertY = false;
+  const ray = t.context.flat.rotate(Math.PI / 2, new Point(3, 4));
+  t.true(ray.from.equals(new Point(3, 4)));
+  t.true(ray.direction.equals(new Vector(0,-1)));
+});
+
+test('translate: correctly translates origin point', t => {
+  const ray = t.context.flat.translate(new Vector(3,4));
+  t.true(ray.from.equals(new Point(6, 8)));
+  t.true(ray.direction.equals(new Vector(1,0)));
+});
+
--- src/lib/ray.ts
+++ src/lib/ray.ts
@@ -1,18 +1,46 @@
+import { Plane } from './plane';
 import { Point } from './point';
+import { Transform } from './transform';
 import { Vector } from './vector';
 
+/**
+ * A ray is a [[Line]] of infinite length. It has a start point ([[from]]) and a direction ([[direction]]) but no end point.
+ */
 export class Ray {
-  private _from: Point;
-  private _direction: Vector;
+  // -----------------------
+  // STATIC
+  // -----------------------
+
+  /**
+   * Returns a new plane centered on the `origin` point and with the [[xAxis]] aligned to `axisPoint`.
+   * @category Create
+   * @param origin      The center of the plane.
+   * @param axisPoint   A point on the xAxis.
+   */
+  public static fromPoints(from: Point, pointOnRay: Point): Ray {
+    const direction = Vector.fromPoints(from, pointOnRay);
+    return new Ray(from, direction);
+  }
+
+  // -----------------------
+  // VARS
+  // -----------------------
+
+  private readonly _from: Point;
+  private readonly _direction: Vector;
+
+  // -----------------------
+  // CONSTRUCTOR
+  // -----------------------
 
   constructor(from: Point, direction: Vector) {
     this._from = from;
     this._direction = direction.unitize();
   }
 
-  /***************************
-   * GET
-   ***************************/
+  // -----------------------
+  // GET
+  // -----------------------
 
   get from(): Point {
     return this._from;
@@ -22,9 +50,9 @@ export class Ray {
     return this._direction;
   }
 
-  /***************************
-   * PUBLIC
-   ***************************/
+  // -----------------------
+  // PUBLIC
+  // -----------------------
 
   /**
    * Gets point a set distance from the start of the ray.
@@ -38,17 +66,102 @@ export class Ray {
     );
   }
 
-  public closestParameter(point: Point): number {
+  public closestParameter(point: Point, onlyPositive: boolean = false): number {
     const xDelta = this._direction.x;
     const yDelta = this._direction.y;
     const u =
       ((point.x - this._from.x) * xDelta + (point.y - this._from.y) * yDelta) /
       (xDelta * xDelta + yDelta * yDelta);
 
+    if(onlyPositive) {
+      if(u < 0) {
+        return 0;
+      }
+    }
     return u;
   }
 
-  public closestPoint(point: Point): Point {
-    return this.pointAt(this.closestParameter(point));
+  public closestPoint(point: Point, onlyPositive: boolean = false): Point {
+    return this.pointAt(this.closestParameter(point, onlyPositive));
+  }
+
+  public withFrom(newFrom: Point): Ray {
+    return new Ray(newFrom, this._direction);
+  }
+
+  public withDirection(newDirection: Vector): Ray {
+    return new Ray(this._from, newDirection);
+  }
+
+  // -----------------------
+  // TRANSFORMABLE
+  // -----------------------
+
+  /**
+   * Returns a copy of the plane transformed by a [[Transform]] matrix.
+   *
+   * Note: If you're applying the same transformation a lot of geometry,
+   * creating the [[Transform]] matrix once and calling this function is faster
+   * than using the direct methods.
+   *
+   * ### Example
+   * ```js
+   * const plane = new Plane(new Point(2,2), Vector.worldX());
+   * console.log(plane.origin);
+   * // => [2,2]
+   *
+   * const mover = Tranform.translate(new Vector(3,4));
+   * const moved = plane.transform(mover);
+   * console.log(moved.origin);
+   * // => [5,6]
+   *
+   * // Direct method
+   * const otherMoved = plane.translate(new Vector(3,4));
+   * console.log(otherMoved.origin);
+   * // => [5,6]
+   * ```
+   *
+   * @category Transform
+   * @param change  A [[transform]] matrix to apply to the BoundingBox.
+   */
+  public transform(change: Transform): Ray {
+    const from = change.transformPoint(this._from);
+    const direction = change.transformVector(this._direction);
+    return new Ray(from, direction);
+  }
+
+  /**
+   * Returns a copy of the plane rotated about a point.
+   * @category Transform
+   * @param angle   Angle of rotation, in radians. If positive, rotates clockwise. If negative, rotates counter clockwise.
+   * @param pivot   Pivot point for rotation. If undefined, the object will be rotated about 0,0.
+   */
+  public rotate(angle: number, pivot?: Point | undefined): Ray {
+    const tran = Transform.rotate(angle, pivot);
+    return this.transform(tran);
+  }
+
+  /**
+   * Returns a copy of the plane translated from one planar coordinate system to another.
+   * In other words, if the plane is described relative to `planeFrom`, after
+   * translation, it will be described relative to `planeTo`.
+   * @category Transform
+   * @param planeFrom   The coordinate system the plane is currently described relative to.
+   * @param planeTo     The coordinate system to describe the plane relative to.
+   */
+  public changeBasis(planeFrom: Plane, planeTo: Plane): Ray {
+    const tran = Transform.changeBasis(planeFrom, planeTo);
+    return this.transform(tran);
+  }
+
+  /**
+   * Returns a copy of this plane translated in a certain direction.
+   * @category Transform
+   * @param move      Direction to move the Plane.
+   * @param distance  Distance to move the Plane. If undefined, will use length of `move` vector.
+   */
+  public translate(move: Vector, distance?: number | undefined): Ray {
+    const tran = Transform.translate(move, distance);
+    return this.transform(tran);
   }
 }
